b882e3a1de2846381672163a1063b7d27c6d2422
Parikshit Patel <parikshit25@yahoo.com>
2018-04-18 22:08:18 -0400

    Commit with Material and Flash mod

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { MaterialModule } from '@angular/material'; 
+import { FlexLayoutModule } from '@angular/flex-layout';
 
+
+
+import 'hammerjs';
 
 import { AppComponent } from './app.component';
+
 
 
 @NgModule({
@@ -10,7 +17,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
